ae2c45cff0b54484cfae6eafda081ddae4a1e7c3
Andrew Pouliot <andrew@darknoon.com>
2012-07-21 12:29:06 -0700

    Added a simple log to test git.

--- Condescension/ConAppDelegate.m
+++ Condescension/ConAppDelegate.m
@@ -15,6 +15,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+	
+	NSLog(@"Hello World");
+	
     return YES;
 }
 							
